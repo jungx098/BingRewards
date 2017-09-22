a311a6a2ed2744fb3fb7d029aaf189626b48eab8
jungx098 <jungx098@gmail.com>
2017-09-21 20:16:53 -0700

    Random search
    The program does random search regardless of the result of
    dashboard parse.

--- pkg/bingRewards.py
+++ pkg/bingRewards.py
@@ -113,7 +113,12 @@ class BingRewards:
             s = page.find('>', s) + 1
             e = page.find(' ', s)
             points = page[s:e]
-            return int(points.replace(",", "")) # remove commas so we can cast as int
+            points = int(points.replace(",", "")) # remove commas so we can cast as int
+
+            if True == points.isdigit(): return int(points)
+
+        #should never happen...
+        return 0
 
     def getDashboardPage(self):
         """
@@ -202,6 +207,9 @@ class BingRewards:
         rewardsText = page[s:e]
         if rewardsText == 'Rewards': # The account is banned
             raise helpers.BingAccountError("Banned from BingRewards: Could not get the number of rewards.")
+        elif True != rewardsText.isdigit():
+            print "Reward point parsing failure: " + rewardsText
+            return 0
         else:
             return int(rewardsText)
 
@@ -328,8 +336,9 @@ class BingRewards:
 
         res = self.RewardResult(reward)
         if reward.isAchieved():
-            res.message = "This reward has been already achieved"
-            return res
+            # res.message = "This reward has been already achieved"
+            # return res
+            print "This reward has been already achieved"
 
         indCol = bdp.Reward.Type.Col.INDEX
 
@@ -344,23 +353,35 @@ class BingRewards:
         matchesMobile = False
         matches = bdp.Reward.Type.SEARCH_AND_EARN_DESCR_RE.search(reward.description)
         # Mobile description changed, so check that one too
-        if matches is None:
+        if (reward.isAchieved() is False) and (matches is None):
             matches = bdp.Reward.Type.SEARCH_AND_EARN_DESCR_RE_MOBILE.search(reward.description)
             matchesMobile = True
-        if matches is None:
-            print "No RegEx matches found for this search and earn"
+
+        if reward.isAchieved():
+            print "Reward is achieved, but continue search"
             res.isError = True
-            res.message = "No RegEx matches found for this search and earn"
-            return res
-        maxRewardsCount = int(matches.group(1))
-        rewardsCount    = int(matches.group(2))
-        rewardCost      = 1 # Looks like it's now always X points per one search
-        searchesCount = maxRewardsCount * rewardCost / rewardsCount
+            res.message = " (random search: reward is achieved)"
+
+            # Init searchesCount
+            searchesCount = random.randint(0, 5)
+        elif matches is None:
+            print "No RegEx matches found for this search and earn"
+            matchesMobile = False
+            res.isError   = True
+            res.message   = " (random search: no RegEx matches found)"
+
+            # Init searchesCount
+            searchesCount = random.randint(1, 5)
+        else:
+            maxRewardsCount  = int(matches.group(1))
+            rewardsCount     = int(matches.group(2))
+            rewardCost       = 1 # Looks like it's now always X points per one search
+            searchesCount    = maxRewardsCount * rewardCost / rewardsCount
 
         # adjust to the current progress
         # reward.progressCurrent is now returning current points, not current searches
         # so divide it by points per search (rewardsCount) to get correct search count needed
-        searchesCount -= (reward.progressCurrent * rewardCost) / rewardsCount
+            searchesCount -= (reward.progressCurrent * rewardCost) / rewardsCount
 
         if matchesMobile == True:
             # new mobile search description gives total search count + points per search for edge/non-edge
@@ -382,12 +403,16 @@ class BingRewards:
             searchesCount += self.addSearchesDesktop + random.randint(0, self.addSearchesDesktopSalt)
             print
             print "Running PC searches"
+            if reward.isAchieved() == True or matches == None:
+                print "- Random exhaustive search"
             print
         elif reward.tp == bdp.Reward.Type.SEARCH_MOBILE:
             headers["User-Agent"] = self.userAgents.mobile
             searchesCount += self.addSearchesMobile + random.randint(0, self.addSearchesMobileSalt)
             print
             print "Running mobile searches"
+            if reward.isAchieved() == True or matches == None:
+                print "- Random exhaustive search"
             print
         else:
             res.isError = True
@@ -487,9 +512,11 @@ class BingRewards:
             i += 1
 
         if successfulQueries < searchesCount:
-            res.message = str(successfulQueries) + " out of " + str(searchesCount) + " requests were successfully processed"
+            res.message = str(successfulQueries) + " out of " + str(searchesCount) + " requests were successfully processed" + \
+                          res.message
         else:
-            res.message = "All " + str(successfulQueries) + " requests were successfully processed"
+            res.message = "All " + str(successfulQueries) + " requests were successfully processed" + \
+                          res.message
 
         # reset header to pc so pc pages return in getting life time points
         headers["User-Agent"] = self.userAgents.pc
@@ -501,10 +528,15 @@ class BingRewards:
         Runs an action for each of rewards as described in self.RewardType
         returns results list of self.RewardResult objects
         """
+
+        # If rewards is not valid, set rewards to an empty list for the
+        # subsequent process to continue without rewards.
         if rewards is None or not isinstance(rewards, list):
-            raise TypeError("rewards is not an instance of list")
+            print "rewards is not an instance of list"
+            rewards = list()
 
         results = []
+        searchCnt = 0
 
         for r in rewards:
             if r.tp is None:
@@ -519,12 +551,58 @@ class BingRewards:
             elif action == bdp.Reward.Type.Action.WARN:
                 res = self.__processWarn(r)
             elif action == bdp.Reward.Type.Action.SEARCH:
-                res = self.__processSearch(r, verbose)
+                if r.isAchieved():
+                    res = self.RewardResult(r)
+                    res.message = "This reward has been already achieved"
+                else:
+                    res = self.__processSearch(r, verbose)
+                    searchCnt = searchCnt + 1
             else:
                 res = self.RewardResult(r)
 
             res.action = action
             results.append(res)
+
+        if searchCnt != 0: return results
+
+        # If no search is performed, do random search
+        # Random PC searches
+        if random.random() < 0.25:
+            searchCnt = 0
+            oldPt     = 0
+            newPt     = self.getRewardsPoints()
+
+            while oldPt != newPt and searchCnt < 10:
+                searchCnt = searchCnt + 1
+                oldPt     = newPt
+
+                r          = bdp.Reward()
+                r.tp       = bdp.Reward.Type.SEARCH_PC
+                res        = self.__processSearch(r, verbose)
+                res.action = bdp.Reward.Type.Action.SEARCH
+
+                results.append(res)
+
+                newPt = self.getRewardsPoints()
+
+        # Random mobile searches
+        if random.random() < 0.25:
+            searchCnt = 0
+            oldPt     = 0
+            newPt     = self.getRewardsPoints()
+
+            while oldPt != newPt and searchCnt < 10:
+                searchCnt = searchCnt + 1
+                oldPt     = newPt
+
+                r          = bdp.Reward()
+                r.tp       = bdp.Reward.Type.SEARCH_MOBILE
+                res        = self.__processSearch(r, verbose)
+                res.action = bdp.Reward.Type.Action.SEARCH
+
+                results.append(res)
+
+                newPt = self.getRewardsPoints()
 
         return results
 
@@ -549,6 +627,7 @@ class BingRewards:
         throws TypeError if rewards is None or not instance of list
         """
         if rewards is None or not isinstance(rewards, list):
+            return None # Do not stop for the process to continue
             raise TypeError("rewards is not an instance of list")
 
         i = 0
